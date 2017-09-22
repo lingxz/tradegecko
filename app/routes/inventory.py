b47b8ec4ccd73361331eb8e17001b16da260a329
lingxz <lingyihuu@gmail.com>
2017-09-22 21:35:11 +0800

    add route for inventory query

--- app/routes/inventory.py
+++ app/routes/inventory.py
@@ -1,7 +1,7 @@
 import os
 import json
 from app import utils
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from app import constants
 
 inventory = Blueprint('inventory', __name__)
@@ -26,5 +26,13 @@ def upload_file():
         abort(400)
     if file and allowed_file(file.filename):
         file.save(constants.DATA_FILE)
-    utils.process_csv(infile=constants.DATA_FILE, outfile=constants.OUTPUT_FILE)
+    utils.process_csv(infile=constants.DATA_FILE)
     return json.dumps({'success': True}), 200, {'ContentType':'application/json'}
+
+@inventory.route('/item')
+def get_prev_state():
+    object_type = request.args.get('object_type', type=str)
+    object_id = request.args.get('object_id', type=int)
+    timestamp = request.args.get('timestamp', type=int)
+    current_state = utils.get_past_state(object_type, object_id, timestamp)
+    return json.dumps(current_state)
